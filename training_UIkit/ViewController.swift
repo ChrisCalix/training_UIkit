6ebbdbce5ee0dde6f870c331db6122c26816f2f3
christian.sonic <yoh.chrisitan@gmail.com>
2023-04-30 20:51:58 -0400

    Initial Commit

---
+++ training_UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  training_UIkit
+//
+//  Created by Sonic on 30/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
